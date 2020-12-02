fa7e1f25f9be837cada3c4a353b4ecf39f378133
Josep Estudis <josepe@gmail.com>
2020-12-02 15:51:48 +0100

    twig event subscriber header all conferences

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -17,15 +17,13 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
-        ]);
+        return $this->render('conference/index.html.twig');
     }
 
     /**
      * @Route("/conference/{id}", name="conference-view")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $commentsPaginated = $commentRepository->getCommentPaginator($conference, $offset);
