8fe459a9963c668968288100262b67eadbf941fe
josep <josepe@gmail.com>
2020-12-06 09:43:53 +0100

    rbmq messaging setup

--- Makefile
+++ Makefile
@@ -4,3 +4,23 @@ tests:
 	symfony console doctrine\:fixtures\:load -n
 	symfony php bin/phpunit
 .PHONY: tests
+
+
+# BACKUP DB COMMAND LINE
+# symfony run pg_dump --data-only > dump.sql
+
+# RESTORE DB COMMAND LINE
+# symfony run psql < dump.sql
+
+# ACCESO A SERVICIO DB
+# docker-compose exec database psql main
+# symfony run psql
+
+# ASYNC MESSAGE CONSUMER
+# symfony console messenger:consume async -vv
+# guest/ guest 
+# symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async
+# symfony console messenger:failed:show
+# symfony server:status
+# symfony open:local:rabbitmq
+# http://127.0.0.1:32782/#/
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\SpamChecker;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Form\CommentFormType;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use Symfony\Component\Form\FormError;
 use App\Repository\ConferenceRepository;
@@ -13,16 +14,19 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ConferenceController extends AbstractController
 {
     private $entityManager;
+    private $bus;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, MessageBusInterface $bus)
     {
         $this->entityManager = $entityManager;
+        $this->bus = $bus;
     }
 
     /**
@@ -36,38 +40,40 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conference-view")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository, SpamChecker $spamChecker, string $photoDir): Response
-    {
+    public function show(
+        Request $request,
+        Conference $conference,
+        CommentRepository $commentRepository,
+        ConferenceRepository $conferenceRepository,
+        string $photoDir
+    ): Response {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $context = [
-                        'user_ip' => $request->getClientIp(),
-                        'user_agent' => $request->headers->get('user-agent'),
-                        'referrer' => $request->headers->get('referer'),
-                        'permalink' => $request->getUri(),
-                        ];
-            if (2 === $spamChecker->getSpamScore($comment, $context)) {
-                //throw new \RuntimeException('Blatant spam, go away!');
-                $form->addError(new FormError("Blatant spam, go away!"));
-            } else {
-                $comment->setConference($conference);
-
-                if ($photo = $form['photo']->getData()) {
-                    $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
-                    try {
-                        $photo->move($photoDir, $filename);
-                    } catch (FileException $e) {
-                        // unable to upload the photo, give up
-                    }
-                    $comment->setPhotoFilename($filename);
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
                 }
-                $this->entityManager->persist($comment);
-                $this->entityManager->flush();
-                return $this->redirectToRoute('conference-view', ['slug' => $conference->getSlug()]);
+                $comment->setPhotoFilename($filename);
             }
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+                ];
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+
+            return $this->redirectToRoute('conference-view', ['slug' => $conference->getSlug()]);
         }
 
 
