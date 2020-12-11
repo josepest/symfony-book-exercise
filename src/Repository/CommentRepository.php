272b5775a26e10cf9b8e4f9304701f42b935da4f
josep <josepe@gmail.com>
2020-12-11 14:09:40 +0100

    command crons

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -17,6 +18,8 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 class CommentRepository extends ServiceEntityRepository
 {
     const PAGINATOR_PER_PAGE = 2;
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
+
 
     public function __construct(ManagerRegistry $registry)
     {
@@ -36,5 +39,27 @@ class CommentRepository extends ServiceEntityRepository
                    ->getQuery()
                ;
         return new Paginator($query);
+    }
+
+    public function countOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->select('COUNT(c.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :state_rejected or c.state = :state_spam')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'state_rejected' => 'rejected',
+                'state_spam' => 'spam',
+                'date' => new \DateTime(-self::DAYS_BEFORE_REJECTED_REMOVAL.' days'),
+            ]);
     }
 }
