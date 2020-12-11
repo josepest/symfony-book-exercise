9231ea0581f2052d787841a27876f30a42f78fd3
josep <josepe@gmail.com>
2020-12-11 10:54:57 +0100

    command y process

---
+++ src/Command/StepInfoCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class StepInfoCommand extends Command
+{
+    protected static $defaultName = 'app:step:info';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+        $process->mustRun();
+        $output->write($process->getOutput());
+
+        return 0;
+    }
+}
